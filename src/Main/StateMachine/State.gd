e278c6577e99f838dc93063b7aad8872a61ecef0
Mick McCarthy <mick@mccarthy.tech>
2020-04-17 21:15:25 +0100

    Add state class

---
+++ src/Main/StateMachine/State.gd
@@ -0,0 +1,33 @@
+extends Node
+
+class_name State, 'res://assets/2d/icons/state.svg'
+
+onready var _state_machine:= _get_state_machine(self)
+
+var _parent: State = null
+
+func _ready() -> void:
+	yield(owner, 'ready')
+	var parent = get_parent()
+	if parent.is_in_group('state_machine'):
+		_parent = parent
+
+func unhandled_input(event: InputEvent) -> void:
+	return
+
+func process(delta: float) -> void:
+	return
+
+func physics_process(delta: float) -> void:
+	return
+
+func enter(msg: Dictionary = {}) -> void:
+	return
+
+func exit(msg: Dictionary = {}) -> void:
+	return
+
+func _get_state_machine(node: Node) -> Node:
+	if node != null and not node.is_in_group('state_machine'):
+		return _get_state_machine(node.get_parent())
+	return node
---
+++ src/Main/StateMachine/StateMachine.gd
@@ -0,0 +1,3 @@
+extends Node
+
+class_name StateMachine, 'res://assets/2d/icons/state_machine.svg'
