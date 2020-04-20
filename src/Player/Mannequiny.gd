c2551f77a303e53c0767b6548488461ad1bfc0bd
Mick McCarthy <mick@mccarthy.tech>
2020-04-20 21:16:54 +0100

    Add animation state machine

--- src/Player/Mannequiny.gd
+++ src/Player/Mannequiny.gd
@@ -1,3 +1,20 @@
 extends Spatial
 
 class_name Mannequiny
+
+enum States { IDLE, RUN, AIR }
+
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var _playback: AnimationNodeStateMachinePlayback = animation_tree['parameters/playback']
+
+func _ready() -> void:
+	animation_tree.active = true
+
+func transition_to(state_id: int) -> void:
+	match state_id:
+		States.IDLE:
+			_playback.travel('idle')
+		States.Run:
+			_playback.travel('run')
+		_:
+			_playback.travel('idle')
