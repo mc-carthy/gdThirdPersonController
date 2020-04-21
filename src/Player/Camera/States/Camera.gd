4d8944f221454584573f10691e529f08071023ce
Mick McCarthy <mick@mccarthy.tech>
2020-04-21 22:53:12 +0100

    Add camera autorotation

--- src/Player/Camera/States/Camera.gd
+++ src/Player/Camera/States/Camera.gd
@@ -1,6 +1,7 @@
 extends CameraState
 
 export var y_inverted: bool = false
+export var deadzone_backwards: float = 0.3
 export var sensitivity_gamepad: Vector2 = Vector2(2.5, 2.5)
 export var sensitivity_mouse: Vector2 = Vector2(0.1, 0.1)
 
@@ -22,6 +23,13 @@ func process(delta: float) -> void:
 	if look_direction.length() > 0.0:
 		update_rotation(look_direction * sensitivity_gamepad * delta)
 	
+	var is_in_deadzone: bool = (
+		move_direction.x >= -deadzone_backwards and move_direction.x <= deadzone_backwards	
+	)
+	
+	if not is_in_deadzone:
+		auto_rotate(move_direction)
+	
 	camera_rig.rotation.y = wrapf(camera_rig.rotation.y, -PI, PI)
 
 func update_rotation(offset: Vector2) -> void:
@@ -29,6 +37,15 @@ func update_rotation(offset: Vector2) -> void:
 	camera_rig.rotation.x += offset.y * -1.0 if y_inverted else offset.y
 	camera_rig.rotation.x = clamp(camera_rig.rotation.x, -0.75, 1.25)
 	camera_rig.rotation.z = 0
+
+func auto_rotate(move_direction: Vector3) -> void:
+	var offset: float = camera_rig.player.rotation.y - camera_rig.rotation.y
+	var target_angle: float = (
+		camera_rig.player.rotation.y - 2 * PI if offset > PI
+		else camera_rig.player.rotation.y + 2 * PI if offset < -PI
+		else camera_rig.player.rotation.y
+	)
+	camera_rig.rotation.y = lerp(camera_rig.rotation.y, target_angle, 0.015)
 
 static func get_look_direction() -> Vector2:
 	return Vector2(
